56908609cad35c0f2bb09fdb23b361e20ea19062
PBShin <49408402+PBShin@users.noreply.github.com>
2019-04-08 11:57:25 -0500

    Add files via upload

---
+++ characterpicturegrid.py
@@ -0,0 +1,14 @@
+#converts list in list variable to transposed string "picture"
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+for y in range(6):
+    for x in range (9):
+        print(grid[x][y],end='')
+    print('')
