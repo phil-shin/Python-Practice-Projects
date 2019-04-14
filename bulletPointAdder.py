f7c982f9017760436c89ca7c6b98445a0744e899
Phil Shin <49408402+PBShin@users.noreply.github.com>
2019-04-14 18:48:21 -0400

    Add files via upload

---
+++ bulletPointAdder.py
@@ -0,0 +1,15 @@
+#! python
+# bullletPointAdder.py - adds wikipedia bullet points to the start
+# of each line of text on the clipboard.
+
+import pyperclip
+text = pyperclip.paste()
+
+# Separate lines and add stars.
+print('a')
+lines = text.split('\n')
+for i in range(len(lines)):   #loop through all indexes in the "lines" list
+    lines[i] = '* ' + lines[i] # add star to each string in "lines" list
+text = '\n'.join(lines)
+pyperclip.copy(text)
+
