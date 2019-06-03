35dd2291ddc3a1d1766c54c47a8d91f9a6d77201
Phil Shin <49408402+PBShin@users.noreply.github.com>
2019-06-03 15:22:50 -0500

    Add files via upload

---
+++ jobReferFormFill.py
@@ -0,0 +1,28 @@
+#! python
+# jobReferFormFill.py - Fills out job referral form at open window
+# and hits submit
+
+import pyautogui, time
+
+# Set First name form and Send button coordinates
+firstNameForm= (864, 480)
+sendButton= (1166, 702)
+
+# Set Form Info.
+formData = {'First Name': 'Tristan', 'Last Name':'Rericha', \
+            'Email':'trericha.jobs@gmail.com', 'Phone':'208-859-5347',\
+            'Country':'united state'}
+
+# Fill out form
+print("Entering %s's info..." % formData['First Name'])
+pyautogui.click(firstNameForm[0], firstNameForm[1])
+pyautogui.typewrite(formData['First Name']+'\t')
+pyautogui.typewrite(formData['Last Name']+'\t')
+pyautogui.typewrite(formData['Email']+'\t')
+pyautogui.typewrite(formData['Phone']+'\t')
+pyautogui.typewrite(formData['Country'])
+
+# Click Send button
+pyautogui.click(sendButton[0], sendButton[1])
+
+print('Done')
