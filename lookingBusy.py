4a43f3d3a367e386c2d2ab971ac71c53dc834a88
Phil Shin <49408402+PBShin@users.noreply.github.com>
2019-06-02 21:09:12 -0500

    Add files via upload

--- lookingBusy.py
+++ lookingBusy.py
@@ -8,10 +8,11 @@ print('Press cntrl + C to end program.')
 # Set program to run every 10 minutes
 while True:
     try:
+        # Moves mouse right to left 1 pixel every 10 minutes
         pyautogui.moveRel(1,0)
         time.sleep(datetime.timedelta(minutes=10).total_seconds())
         pyautogui.moveRel(-1,0)
-        time.sleep(datetime.timedelta(minutes=1).total_seconds())
+        time.sleep(datetime.timedelta(minutes=10).total_seconds())
         # Allow for cntl+C to interrupt program
     except KeyboardInterrupt:
         print('Program Ended')
